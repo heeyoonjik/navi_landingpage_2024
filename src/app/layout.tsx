683bbb425db24533a60db231a4d35680177a44b2
_yoonyoon_1 <heeyoon1302@naver.com>
2024-01-19 11:54:50 +0900

    재배포 위한 커밋

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body>
         <Header />
         <main>{children}</main>
